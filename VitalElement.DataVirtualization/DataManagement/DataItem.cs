55d0480b1c0855652552bd4927d1a821d304f417
Dan Walmsley <dan@walms.co.uk>
2024-04-13 00:12:10 +0100

    no placeholders needed.

--- VitalElement.DataVirtualization/DataManagement/DataItem.cs
+++ VitalElement.DataVirtualization/DataManagement/DataItem.cs
@@ -6,19 +6,77 @@ using System.Runtime.CompilerServices;
 
 public interface IDataItem
 {
-    object? Item { get; }
+    object Item { get; internal set; }
 }
 
-public class DataItem<T> : IDataItem, INotifyPropertyChanged
+public class DataItem<T> : IDataItem, INotifyPropertyChanged where T : class
 {
     private T _item;
+    private bool _isLoading = true;
 
     public DataItem(T item)
     {
         _item = item;
     }
 
-    object? IDataItem.Item => Item;
+    public bool IsLoading
+    {
+        get => _isLoading;
+        private set
+        {
+            if (value == _isLoading) return;
+            _isLoading = value;
+            OnPropertyChanged();
+        }
+    }
+
+    object IDataItem.Item
+    {
+        get => Item;
+        set
+        {
+            IsLoading = false;
+            Item = (T)value;
+        }
+    }
+
+    public T Item
+    {
+        get => _item;
+        private set
+        {
+            _item = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool IsInUse
+    {
+        get { return PropertyChanged != null; }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
+public class DataItem1<T> : IDataItem, INotifyPropertyChanged
+{
+    private T _item;
+
+    public DataItem1(T item)
+    {
+        _item = item;
+    }
+
+    object IDataItem.Item
+    {
+        get => Item;
+        set => Item = (T)value;
+    }
 
     public T Item
     {
